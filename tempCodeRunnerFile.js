d3bcd7cc4a28f163c84e9377c4487fbe40f420c6
nafiz <naafizrahman1@gmail.com>
2020-06-06 22:50:09 +0600

    CSV to Json conversion done

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const csvFilePath = "./sample.csv";
-const csv = require("csvtojson");
-
-const jsonArray = await csv().fromFile(csvFilePath);
-
-var i;
-for (i = 0; i < 1; i++) {
-  console.log(jsonArray[i]);
-}
