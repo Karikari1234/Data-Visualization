fba26bc51582ca1db07435239ea0a7839943690b
nafiz <naafizrahman1@gmail.com>
2020-06-12 22:52:15 +0600

    Successfully being able to render data from backend to the frontend using fetch API

--- public/app.js
+++ public/app.js
@@ -4,13 +4,27 @@ d3.select("div")
   .enter()
   .append("p")
   .text((data) => data);
-
+var sampleData;
+const myDemo = document.getElementById("demo");
 fetch("data/")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    sampleData = data;
+    console.log("Success");
+    var count = 0;
+    for (data in sampleData) {
+      if (data <= 10) {
+        let listItem = document.createElement("li");
+        listItem.classList.add("listItem");
+        listItem.innerHTML =
+          sampleData[data].Value + " " + sampleData[data].Variable_name;
+        myDemo.appendChild(listItem);
+        console.log(data);
+      } else {
+      }
+    }
   })
   .catch(function (err) {
     console.warn("Something went wrong.", err);
