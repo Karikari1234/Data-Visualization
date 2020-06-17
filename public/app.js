c3acd236cbb5fa1b4c93cd1d62002127d543e536
nafiz <naafizrahman1@gmail.com>
2020-06-17 17:32:34 +0600

    Basic Buttons setup for fetching data and clearing them

--- public/app.js
+++ public/app.js
@@ -1,21 +1,36 @@
 const myDemo = document.getElementById("demo");
+const dataButton = document.querySelector(".data-btn");
+const clearButton = document.querySelector(".clear-btn");
+let i = 0;
 
-fetch("data/")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    for (i in data) {
-      if (i < 10) appendData(myDemo, data[i]);
-    }
-  })
-  .catch(function (err) {
-    console.warn("Something went wrong.", err);
-  });
+dataButton.addEventListener("click", function () {
+  fetchData();
+  i += 10;
+});
+
+clearButton.addEventListener("click", function () {
+  myDemo.innerHTML = "";
+});
 
 function appendData(elementId, data) {
   let listItem = document.createElement("li");
   listItem.classList.add("listItem");
   listItem.innerHTML = data.Value + " " + data.Variable_name;
   elementId.appendChild(listItem);
+}
+
+function fetchData() {
+  fetch("data/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (j = i; j < i + 10; j++) {
+        appendData(myDemo, data[j]);
+      }
+      i += 10;
+    })
+    .catch(function (err) {
+      console.warn("Something went wrong.", err);
+    });
 }
